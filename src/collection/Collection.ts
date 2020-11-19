53a5f176bb988d286c6c4c6f1cf6a31a500021fa
thetayloredman <nutdriver716@gmail.com>
2020-11-19 08:51:56 -0800

    Change licence header to my name

--- src/collection/Collection.ts
+++ src/collection/Collection.ts
@@ -15,3 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+
+export class Collection<K, V> extends Map<K, V> {
+    public constructor(data: [K, V][]) {
+        super(data ?? []);
+    }
+}
